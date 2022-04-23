a817c8c05687a59100b36d22c41c33b265c3f89e
tomasblomgren <Tomas.blomgren96@gmail.com>
2022-04-23 12:03:29 +0000

    adding empty functions

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+function rungame() {
+
+}
+
+function shufflecards() {
+
+}
+
+function stay () {
+
+}
+
+function another card() {
+
+}
+function give point() {
+    
+}
+function add round() {
+    
+}
+function end game() {
+    
+}
+function calculate winner() {
+    
+}
+function () {
+    
+}
