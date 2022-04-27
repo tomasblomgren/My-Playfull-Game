700b10341aaaa87e43874cdb32362f93c77e023c
tomasblomgren <Tomas.blomgren96@gmail.com>
2022-04-27 14:41:28 +0000

    adding callback and functions to the game buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,173 @@
 /* adding Eventlisteners and DOM*/
 
+
+
 document.addEventListener ("DOMContentLoaded", function (){
 let buttons = document.getElementsByTagName ("button");
-
+var content = document.getElementById("content");
+var button = document.getElementById("rungame");
+var startButton = document.getElementById("startgame");
+startButton.addEventListener("click", rungame);
 for (let button of buttons) {
     button.addEventListener("click", function(){
         if (this.getAttribute("data-type")=== "submit") {
             alert("you clicked sumbit!");
-        } else {
-            let gametype = this.getAttribute("data-type");
-            alert('You clicked '${gametype})
-        }
+        } 
     })
 }
 })
 
 function rungame() {
-
-// Creates two random numbers between 1 and 10
-let num1 = Math.floor(Math.random() * 10) + 1;
-let num2 = Math.floor(Math.random() * 10) + 1;
-
+console.log("gameshouldstart");    
 
 }
+ 
 
-function shufflecards() {
+function gamearea() {
+document.getElementById("gamearea").addEventListener("click",function(gamearea)
 
+{ 
+    console.log("gamearea clicked");
+});
 }
+
+document.addEventListener ("DOMContentLoaded", function (){
+    let buttons = document.getElementsByTagName ("button");
+    var content = document.getElementById("content");
+    var button = document.getElementById("stay");
+    var startButton = document.getElementById("nomorecards");
+    startButton.addEventListener("click", stay);
+    
+    });
+
 
 function stay () {
+console.log("nomorecardsallowed");
+}
+document.addEventListener ("DOMContentLoaded", function (){
+    let buttons = document.getElementsByTagName ("button");
+    var content = document.getElementById("content");
+    var button = document.getElementById("card");
+    var startButton = document.getElementById("hit");
+    startButton.addEventListener("click", card);
+    });
+    
 
+function card () {
+  // Creates two random numbers between 1 and 10
+  let num1 = Math.floor(Math.random() * 11) + 1;
+  let num2 = Math.floor(Math.random() * 11) + 1;
+
+console.log("recieve card")
 }
 
-function another card (operand1, operand2) {
+document.addEventListener ("DOMContentLoaded", function (){
+    let buttons = document.getElementsByTagName ("button");
+    var content = document.getElementById("content");
+    var button = document.getElementById("split");
+    var startButton = document.getElementById("divideamount");
+    startButton.addEventListener("click", split);
+    
+    });
 
+
+function split () {
+console.log("divide full amount")    
 }
-function give point () {
-    if (calculate winner === "end game")
-}
-function add round () {
+
+
+document.addEventListener ("DOMContentLoaded", function (){
+    let buttons = document.getElementsByTagName ("button");
+    var content = document.getElementById("content");
+    var button = document.getElementById("givepoint");
+    var startButton = document.getElementById("hit");
+    startButton.addEventListener("click", givepoint);
+   
+    });
+    function givepoint () {
+
+    }
+    document.addEventListener ("DOMContentLoaded", function (){
+        let buttons = document.getElementsByTagName ("button");
+        var content = document.getElementById("content");
+        var button = document.getElementById("shufflecards");
+        var startButton = document.getElementById("randomisecars");
+        startButton.addEventListener("click", shufflecards);
+        
+        });
+    
+    
+    function shufflecards () {
+    console.log("shuffles cards");
+    }
+
+document.addEventListener ("DOMContentLoaded", function (){
+    let buttons = document.getElementsByTagName ("button");
+    var content = document.getElementById("content");
+    var button = document.getElementById("card");
+    var startButton = document.getElementById("hit");
+    startButton.addEventListener("click", card);
+    for (let button of buttons) {
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type")=== "submit") {
+                alert("you clicked submit!");
+            } 
+        })
+    }
+    })
+function addround () {
     
 }
-function end game () {
+document.addEventListener ("DOMContentLoaded", function (){
+    let buttons = document.getElementsByTagName ("button");
+    var content = document.getElementById("content");
+    var button = document.getElementById("card");
+    var startButton = document.getElementById("hit");
+    startButton.addEventListener("click", card);
+    for (let button of buttons) {
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type")=== "submit") {
+                alert("you clicked submit!");
+            } 
+        })
+    }
+    })
+function endgame () {
     
 }
-function calculate winner () {
+document.addEventListener ("DOMContentLoaded", function (){
+    let buttons = document.getElementsByTagName ("button");
+    var content = document.getElementById("content");
+    var button = document.getElementById("card");
+    var startButton = document.getElementById("hit");
+    startButton.addEventListener("click", card);
+    for (let button of buttons) {
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type")=== "submit") {
+                alert("you clicked submit!");
+            } 
+        })
+    }
+    })
+    
+function calculatewinner () {
     
 }
-function lose game () {
+document.addEventListener ("DOMContentLoaded", function (){
+    let buttons = document.getElementsByTagName ("button");
+    var content = document.getElementById("content");
+    var button = document.getElementById("card");
+    var startButton = document.getElementById("hit");
+    startButton.addEventListener("click", card);
+    for (let button of buttons) {
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type")=== "submit") {
+                alert("you clicked submit!");
+            } 
+        })
+    }
+    })
+function losegame () {
     
 }
+
