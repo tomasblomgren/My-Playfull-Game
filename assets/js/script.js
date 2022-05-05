0e5dc783f68f4a0b3820a69ccf6d95e7c3a02981
tomasblomgren <Tomas.blomgren96@gmail.com>
2022-05-05 18:46:25 +0000

    changing the shuffle button to an onload function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var dealerSum = 0;
 var playerSum = 0;
 
 var dealerAceCount = 0;
-var yourAceCount = 0; // A, 2 + K -> 1 + 2 + 10 
+var yourAceCount = 0; 
 
 var hidden; 
 var deck;
@@ -13,37 +13,32 @@ var canHit = true; /* allows player to draw while yourSum < = 21*/
 
 window.onload = function () {
     buildDeck ();
+    shuffleDeck();
+    startGame();
 }
 
-function buildDeck (){
-    /*let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+function buildDeck() {
+    let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
     let types = ["C", "D", "H", "S"];
-    deck = [];*/
+    deck = [];
 
+    for (let i = 0; i < types.length; i++) {
+        for (let j = 0; j < values.length; j++) {
+            deck.push(values[j] + "-" + types[i]); // A-C -> K-C, A-D, K-D
+        }
+    }
+    console.log(deck); 
 }
 
-/*rendering deck */
-function renderDeck(deck)
-{
-      document.getElementById("deck").innerHTML = "";
-
-	for(let i = 0; i < deck.length; i++)
-	{
-		let card = document.createElement("div");
-		let value = document.createElement("div");
-		let suit = document.createElement("div");
-		card.className = "card";
-		value.className = "value";
-		suit.className = "suit " + deck[i].Suit;
-
-		value.innerHTML = deck[i].Value;
-		card.appendChild(value);
-		card.appendChild(suit);
-
-		document.getElementById("deck").appendChild(card);
-	}
+function shuffleDeck() {
+    for (let i = 0; i < deck.length; i++) {
+        let j = Math.floor(Math.random() * deck.length); // (0-1) * 52 = => (0-51.9999)
+        let temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+    }
+    console.log(deck);
 }
- 
 
 /*defining deck of cards*/
 var suits = ["Spades", "Diamonds", "Clubs", "Hearts"];
@@ -60,7 +55,7 @@ class blackjack {
     
 }
 
-function generateDeck(){
+function generateDeck() {
     gameDeck = getDeck();
 }
 
@@ -86,7 +81,7 @@ document.addEventListener ("DOMContentLoaded", function (){
 
 hidden = deck.pop();
 dealerSum += getValue(hidden);
-dealerAceCount += checkAce
+dealerAceCount += checkAce(hidden);
 console.log(hidden);
 console.log(dealerSum);
 
@@ -99,20 +94,21 @@ function getValue(card) {
     let data = card.split("-"); //"4-C" -> ["4", "C"]
     let value = data[0];
 
-    if (isNaN(value)) {
+    if (isNaN(value)) { // A J Q K 
     if (value == "A") {
-        return 11;}    
-    }
-return 10;
-}
-{
-return parseInt(value); 
+        return 11;
+        }    
+    
+    return 10;
+  }
+return parseInt(value);
 }
 
 function checkAce(card) {
-    if (card)[0] == "A") {
+    if (card[0] == "A") {
         return 1;
-    } {
+    } 
+    {
         return 0;
     }
 } 
@@ -192,8 +188,7 @@ document.addEventListener ("DOMContentLoaded", function (){
     */
     }
     document.addEventListener ("DOMContentLoaded", function (){
-        let buttons = document.getElementsByTagName ("button");
-        var content = document.getElementById("content");
+        
         var button = document.getElementById("shufflecards");
         var startButton = document.getElementById("randomisecards");
         startButton.addEventListener("click", shufflecards);    
